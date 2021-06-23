bf22832766d0dc9b5cb33401e0d5cd9948f3bc2f
Deshan-Chathuranga <deshanc321@gmail.com>
2021-06-23 17:25:52 +0530

    added appbar to homescreen

--- src/screens/todolist.js
+++ src/screens/todolist.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { ScrollView, Text, View, Button } from "react-native";
+import { ScrollView, Text, View, Button,StyleSheet } from "react-native";
 import * as SecureStore from "expo-secure-store";
+import {Appbar} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const todoList = ({navigation}) => {
     const {navigate}=navigation;
@@ -8,8 +10,13 @@ const todoList = ({navigation}) => {
     return await SecureStore.getItemAsync(key);
   };
   return (
-    <ScrollView style={{ padding: 20 }}>
-      <Text style={{ fontSize: 27 }}>Welcome</Text>
+    <ScrollView >
+      <Appbar.Header style = {styles.item}>
+     <Appbar.Content title="Home" />
+      {/* <Appbar.Action icon="user" onPress={() => {navigate("Login")}} /> */}
+      <Icon name="user" size={30} color="white" onPress={() => {navigate("Login")}}/>
+  </Appbar.Header>
+      {/* <Text style={{ fontSize: 27 }}>Welcome</Text>
       <View style={{ margin: 20 }} />
       <Button
         onPress={() => {
@@ -19,9 +26,15 @@ const todoList = ({navigation}) => {
         
         }}
         title="Logout"
-      />
+      /> */}
     </ScrollView>
   );
 };
 
 export default todoList;
+const styles = StyleSheet.create ({
+  item: {
+     backgroundColor : "#00716F",
+     width:"auto"
+  }
+})
